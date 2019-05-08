a56f851858edff1f9b47f3b0dd661c3187310231
cric <gianluca.aguzzi@studio.unibo.it>
2019-05-08 14:46:33 +0200

    add javadoc

--- src/main/java/Mouse.java
+++ src/main/java/Mouse.java
@@ -1,3 +1,9 @@
+/**
+ * mouse is what do you expected.
+ * it can move or eat.
+ * if it eats a lot of food, it can die.
+ * please be careful with this little creature.
+ */
 public class Mouse {
     private static final int MAX_HEALTH = 100;
     private static final int DELTA_HEALTH = 10;
@@ -11,6 +17,10 @@ public class Mouse {
         this.lifeStatus = MAX_HEALTH;
     }
 
+    /**
+     * eat something found in the environment
+     * @return true if mouse is alive false otherwise
+     */
     public boolean eat() {
         if(!alive) return false;
         this.lifeStatus -= DELTA_HEALTH;
@@ -18,13 +28,17 @@ public class Mouse {
         return true;
     }
 
+    /**
+     * mouse moves randomly in the environment
+     * @return true if mouse is alive false otherwise
+     */
     public boolean moveRandomly() {
         if(!alive) return false;
         this.lifeStatus += DELTA_HEALTH;
         this.alive = verifyIsAlive();
         return true;
     }
-
+    
     public boolean isAlive() {
         return alive;
     }
