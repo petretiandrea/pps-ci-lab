d7112cacf2af280be46d5fb50f56d9244dfbd701
marta.luffarelli <marta.luffarelli@studio.unibo.it>
2019-05-08 14:07:28 +0200

    Bootstrapping project

---
+++ src/main/java/City.java
@@ -0,0 +1,21 @@
+public class City {
+    private final String name;
+    private long inhabitants;
+
+    public City(final String name, final long inhabitants) {
+        this.name = name;
+        this.inhabitants = inhabitants;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public long getInhabitants(){
+        return this.inhabitants;
+    }
+    public void updateInhabitants(final long inhabitants) {
+        if(inhabitants > 1) {
+            this.inhabitants = inhabitants;
+        }
+    }
+}
